706d05df1d5e67d5b3f5d6fa072358128b238cff
The Ninja <69609060+samuraininja360@users.noreply.github.com>
2021-04-08 16:13:25 +1200

    Toggled Canvas Size

--- Game/platformer.js
+++ Game/platformer.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
 const blue = "#0064ff"
 const red = "ff0032"
 const yellow = "ffc800"
@@ -7,9 +8,12 @@ const purple = "7900dc"
 
 function setup() {
   console.log("Setting things up")
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
   update()
 }
 
 function update() {
+  
   window.requestAnimationFrame(update)
 }
