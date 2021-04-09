613e3051cb6226f465f359011d13ce15b800db3d
The Ninja <69609060+samuraininja360@users.noreply.github.com>
2021-04-09 13:10:43 +1200

    Updated JS

--- Game/platformer.js
+++ Game/platformer.js
@@ -1,22 +1,26 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-const blue = "#0064ff"
-const red = "ff0032"
-const yellow = "ffc800"
-const green = "00b428"
-const purple = "7900dc"
+//Platformer
+var container = document.getElementsByTagName("container")[0]
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d")
+var blue = "#0064ff"
+var red = "#ff0032"
+var yellow = "#ffc800"
+var green = "#00b428"
+var purple = "#7900dc"
 
 function setup() {
-  console.log("Setting things up")
-  update()
+    console.log("Setting things up")
+    update()
 }
 
 function update() {
-  ctx.fillStyle = blue
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  window.requestAnimationFrame(update)
+    ctx.canvas.width = window.innerWidth
+    ctx.canvas.height = window.innerHeight
+    ctx.fillStyle = "#0064ff"
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    window.requestAnimationFrame(update)
 }
 
-ctx.canvas.width = window.clientWidth
-ctx.canvas.height = window.clientHeight
+ctx.canvas.width = window.innerWidth
+ctx.canvas.height = window.innerHeight
 setup()
